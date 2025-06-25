093bb0eacdf7eb68cfed3e44428bc18f94c447c5
Alejandro Lopez <alejo32267@gmail.com>
2025-06-25 17:37:02 -0400

    trabajando

--- src/app.js
+++ src/app.js
@@ -1,11 +1,28 @@
-import "bootstrap";
+
 import "./style.css";
 
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  document.querySelector('.card').classList.add('generateRandomSuit');
+  
+  setTimeout(() => {
+    document.querySelector('.card').classList.remove('heart');
+    document.querySelector('.card').classList.add('spade');
+  }, 1000); // 1 segundo
 };
+
+let generateRandomNumber = () => {
+  let numb = Math.floor(Math.random() * 13); // Cambiado a 13 para incluir el "K"
+  let numbers = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "J", "Q", "K"];
+  return numbers[numb]; // Retorna el número aleatorio
+};
+
+let generateRandomSuit = () => {
+  let suits = ["diamond", "spade", "heart", "club"];
+  let indexSuit = Math.floor(Math.random() * suits.length); // Cambiado a suits.length
+  return suits[indexSuit]; // Retorna el palo aleatorio
+};
+
+// Ejemplo de uso
+console.log(generateRandomNumber()); // Muestra un número aleatorio
+console.log(generateRandomSuit()); // Muestra un palo aleatorio
